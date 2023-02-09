0eacef1fd32ae4ba543a7b83a6f76835f4099b8a
Elmurot <elmurotyangiboev@gmail.com>
2023-02-09 03:11:21 -0500

    git push origin main

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student maria = new Student("Elmurot", "Yangiboev", "elmurotyangiboev@gmail.com", 33);
+            studentRepository.save(maria);
+        };
     }
 
 }
